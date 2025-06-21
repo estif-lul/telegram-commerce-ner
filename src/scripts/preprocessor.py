83dbdf73b413fd6dc95851d174f32ce19842c886
est-lul <estlul2004@gmail.com>
2025-06-21 15:30:57 +0300

    Done Preprocessing

---
+++ src/scripts/preprocessor.py
@@ -0,0 +1,42 @@
+import re
+
+def normalize_amharic(text):
+    """
+    Normalize Amharic text by removing redundant characters.
+    """
+    subs = {
+        '[ሀሐኀሃኃ]': 'ሀ', '[ሁሑኁ]': 'ሁ', '[ሂሑኂ]': 'ሂ', '[ሄሔኄ]': 'ሄ', '[ህሕኅ]': 'ህ', '[ሆሖኆ]': 'ሆ',
+        '[ሠሰ]': 'ሰ', '[ሡሱ]': 'ሱ', '[ሢሲ]': 'ሲ', '[ሣሳ]': 'ሳ', '[ሤሴ]': 'ሴ', '[ሥስ]': 'ስ', '[ሦሶ]': 'ሶ',
+        '[ዐአዓኣ]': 'አ', '[ዑኡ]': 'ኡ', '[ዒኢ]': 'ኢ', '[ዔኤ]': 'ኤ', '[ዕእ]': 'እ', '[ዖኦ]': 'ኦ',
+        '[ጸፀ]': 'ጸ', '[ፁጹ]': 'ጹ', '[ፂጺ]': 'ጺ', '[ፃጻ]': 'ጻ', '[ፄጼ]': 'ጼ', '[ፅጽ]': 'ጽ', '[ጾፆ]': 'ጾ',
+        '[ሗኈ]': 'ኈ', '[ሧሷ]': 'ሷ'
+    }
+    for pattern, replacement in subs.items():
+        text = re.sub(pattern, replacement, text)
+    return text.strip()
+
+def remove_emojies(text):
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # Emoticons
+        "\U0001F300-\U0001F5FF"  # Symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # Transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # Flags
+        "\U00002700-\U000027BF"  # Dingbats
+        "\U000024C2-\U0001F251"  # Enclosed characters
+        "\U0001F900-\U0001F9FF"  # Supplemental symbols and pictographs
+        "]+",
+        flags=re.UNICODE
+    )
+    return emoji_pattern.sub(r'', text).strip()
+
+def tokenize(text):
+    # Remove extra punctuation
+    text = re.sub(r'[\"“”።፣.,!?…()]', ' ', text)
+    # Split on whitespace
+    tokens = text.strip().split()
+    return tokens
+
+if __name__ == "__main__":
+    sample_text =  "ዋጋ 1000 ብር 😊📦 በአሠዲስ አበባ 🚚🥮"
+    print(remove_emojies(sample_text))
